e0c21ed14844dc1842bbacfeb7c4b580c91dfacb
afisher93 <106286620+afisher93@users.noreply.github.com>
2022-07-15 19:09:03 -0500

    complete

--- app.js
+++ app.js
@@ -2,6 +2,24 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
-
+for (let i = 0; i< 100; i++) {
+    if (i % 2 == 0) {
+        continue;
+    } else {
+        console.log(i);
+    }
+}
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+for ( let i = 1; i < 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+    console.log("FIZZBUZZ");
+    } else {
+        if (i % 3 == 0){
+            console.log("FIZZ");
+        }
+        if (i % 5 == 0) {
+            console.log("BUZZ");
+        }
+    }
+}
